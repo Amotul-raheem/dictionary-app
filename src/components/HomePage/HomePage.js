6d9ce2647b5aeec225220db48074d37b7cd20b24
Amotul-raheem <amotulraheem1499@gmail.com>
2022-04-28 15:40:13 +0100

    Created the search bar

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "./HomePage.css"
 import Search from "../Search/Search.js";
+import Word from "../Word/Word";
 
 
 function HomePage() {
@@ -14,7 +15,7 @@ function HomePage() {
     return (
         <div className="homepage">
             <div className="homepage-nav-bar">
-                <h1>Home Page</h1>
+                <h1 className="home-page-logo">Home Page</h1>
             </div>
             <div>
                 <Search
@@ -22,6 +23,25 @@ function HomePage() {
                     handleChange={handleChange}
                     handleSearch={handleSearch}
                 />
+            </div>
+            <div>
+                <div>
+                    <Word
+                        date={"12-04-2022"}
+                        meaning={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n" +
+                            "molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n" +
+                            "numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium\n" +
+                            "optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis\n" +
+                            "obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam\n" +
+                            "nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,\n" +
+                            "tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,\n" +
+                            "!"}
+                    />
+                    <div>
+
+                    </div>
+                </div>
+
             </div>
         </div>
 
